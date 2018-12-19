eb22bc435a64a95e323a2221fd16322ee7d7e28b
Andrew Harkins <extremelyexceeding@gmail.com>
2018-12-19 02:45:54 -0600

    Lab06 - Correct Arrays exam pass/fail - Console
    multiple arrays determine number of correct/incorrect/and wrong answers.
    Exam taking user input and getting results

---
+++ Lab06_JavaToCSharp/DriverExam.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab06_JavaToCSharp
+{
+    // DriverExam class
+    class DriverExam
+    {
+        // Array containing the correct answers
+        private char[] correct = { 'B', 'D', 'A', 'A',
+                              'C', 'A', 'B', 'A',
+                              'C', 'D', 'B', 'C',
+                              'D', 'A', 'D', 'C',
+                              'C', 'B', 'D', 'A' };
+        private char[] student;       // The student's answers
+        private int[] missed;         // The missed question numbers
+        private int numCorrect = 0;   // The number correct
+        private int numIncorrect = 0; // The number incorrect
+
+        /**
+         * Constructor
+         * Accepts an array of the student's
+         * answers as the argument. The contents
+         * of the array are copied to the student
+         * field.
+         */
+        public DriverExam(char[] s)
+        {
+            student = s;
+            GradeExam();
+        }
+
+
+        /**
+         * GradeExam method
+         * This method determines the number of
+         * correct and incorrect answers. It calls
+         * the makeMissedArray method.
+         */
+        private void GradeExam()
+        {
+            /* Missing code starts here */
+            for(int i = 0; i < student.Length; i++)
+            {
+                if (student[i] == correct[i])
+                    numCorrect++;
+                else
+                    numIncorrect++;
+            }
+            /* Missing code ends here */
+            MakeMissedArray();
+        }
+
+
+        /**
+         * MakeMissedArray method
+         * This method makes the missed array and
+         * stores the numbers of all the questions
+         * that the student missed in it.
+         */
+        private void MakeMissedArray()
+        {
+            /* Missing code starts here */
+            missed = new int[numIncorrect];     // Declare array so it isn't 'null'
+            int index = 0;                      // Create variable to hold 'index of array'
+            for(int i = 0; i < student.Length; i++)
+            {
+                if(student[i] != correct[i])
+                {
+                    missed[index] = i + 1;
+                    index++;
+                }
+            }
+            /* Missing code ends here */
+        }
+
+
+        /*
+         * Passed method - looks at answers.
+         */
+         public bool Passed()
+        {
+            bool status = false;
+            if (numCorrect >= 15)
+                status = true;
+            else
+                status = false;
+
+            return status;
+        }
+
+
+        /*
+         * TotalCorrect method.
+         */
+         public int TotalCorrect()
+        {
+            return numCorrect;
+        }
+
+
+        /*
+         * TotalIncorrect method.
+         */
+         public int TotalIncorrect()
+        {
+            return numIncorrect;
+        }
+
+
+        /**
+         * questionsMissed method
+         * Returns an array containing the numbers
+         * of the missed questions. If no questions
+         * were missed, returns null.
+         */
+         public int[] QuestionsMissed()
+        {
+            return missed;
+        }
+    }
+}
--- Lab06_JavaToCSharp/Program.cs
+++ Lab06_JavaToCSharp/Program.cs
@@ -7,9 +7,92 @@ using System.Threading.Tasks;
 namespace Lab06_JavaToCSharp
 {
     class Program
-    {
+    {   // DriverTest
         static void Main(string[] args)
         {
+            // Constant for the number of answers.
+            const int NUM_ANSWERS = 20;
+
+            // Array to hold the student's answers.
+            char[] answers = new char[NUM_ANSWERS];
+
+            // This will reference an array holding the
+            // question numbers of the missed questions.
+            int[] missedQuestions = null;
+
+            // Get the user's answers to the questions.
+            Console.WriteLine("Enter your answers to the " +
+                         "exam questions. (Make sure " +
+                         "Caps Lock is ON!)");
+
+            for(int i = 0; i < answers.Length; i++)
+            {
+                Console.WriteLine("Question " + (i + 1) + ": ");
+                string input = Console.ReadLine();
+                
+                if(char.TryParse(input, out char inserted))
+                {
+                    answers[i] = inserted;
+
+                    // Validate the answer.
+                    while (!Valid(answers[i]))
+                    {
+                        Console.WriteLine("ERROR: Valid answers are A, B, C, or D.");
+                        Console.Write("Question " + (i + 1) + ": ");
+                        input = Console.ReadLine();
+                        if(char.TryParse(input, out char userChar))
+                        {
+                            answers[i] = userChar;
+                        }
+                    }
+                }
+            }
+
+            // Create a DriverExam object.
+            DriverExam exam = new DriverExam(answers);
+
+            // Get an array of the missed question numbers.
+            missedQuestions = exam.QuestionsMissed();
+
+            // Display a report.
+            Console.WriteLine("Correct answers: " + exam.TotalCorrect());
+            Console.WriteLine("Incorrect answers: " + exam.TotalIncorrect());
+
+            // Display whether the student passed or failed.
+            if (exam.Passed())
+                Console.WriteLine("You passed the exam!");
+            else
+                Console.WriteLine("You failed the exam.");
+
+            if(missedQuestions != null)
+            {
+                Console.WriteLine("You missed the following questions: ");
+                for(int i = 0; i < missedQuestions.Length; i++)
+                {
+                    Console.Write(missedQuestions[i] + " ");
+                }                
+            }
+
+            Console.ReadKey();
+        }
+
+
+        /*
+         * Valid method
+         * Returns true if argument is A, B,C, or D.
+         */
+        public static bool Valid(char c)
+        {
+            bool status = false;
+
+            if (c == 'A' || c == 'B' || c == 'C' || c == 'D')
+            {
+                status = true;
+            }
+            else
+                status = false;
+
+            return status;
         }
     }
 }
